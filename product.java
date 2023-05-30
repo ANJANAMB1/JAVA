cb06858732165a8db09674f27cf168514bf95806
ANJANAMB1 <92799111+ANJANAMB1@users.noreply.github.com>
2023-05-30 20:16:36 +0530

    Add files via upload

--- product.java
+++ product.java
@@ -10,23 +10,24 @@ public class product
 	pname = pname_get;
 	price = price_get;
 	}
-	public int get_price() 
-	{
-		return price;
-	}
-	public static void main(String[] args) 
-	{
-		product p_1 = new product("A123", "TV", 2);
-		product p_2 = new product("B123", "RADIO", 21);
-		product p_3 = new product("C123", "DVD", 15);
+	 
+	public static void main(String args[])
+        {
+    
+          	product p_1 = new product("A123", "TV", 20000);
+		product p_2 = new product("B123", "RADIO", 12000);
+		product p_3 = new product("C123", "DVD", 15000);
 		if (p_1.price <= p_3.price && p_1.price <= p_2.price)
-			System.out.println("Lowest product price : " + p_1.price);
+			System.out.println("Lowest product price & product name :"+p_1.price +p_1.pname  );
 		if (p_3.price <= p_1.price && p_3.price <= p_2.price)
-			System.out.println("Lowest product price  : " + p_3.price);
+			System.out.println("Lowest product price & product name  : " + p_3.price +p_3.pname );
 		if (p_2.price <= p_3.price && p_2.price <= p_1.price)
-			System.out.println("Lowest product price : " + p_2.price);
-	}
+			System.out.println("Lowest product price & product name  : " + p_2.price +p_2.pname);
+			 
+			}
+			 
 }
+
 
 
 
