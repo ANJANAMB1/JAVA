2c9c968c73c27bcedc9e5f7871458fa93496ad2c
ANJANAMB1 <92799111+ANJANAMB1@users.noreply.github.com>
2023-07-03 13:47:31 +0530

    Update shapes.java

--- shapes.java
+++ shapes.java
@@ -12,18 +12,18 @@ class Circle implements prop
     double pi = 3.14;
     double r;
     Scanner sc = new Scanner(System.in);
-    @Override
+     
     public void getdata()
     {
         System.out.println("Enter the radius of the circle:");
         r = sc.nextDouble();
     }
-    @Override
+     
     public void perimeter()
     {
         System.out.println("Perimeter of the circle: "+(2*pi*r));
     }
-    @Override
+     
     public void area()
     {
         System.out.println("Perimeter of the circle: "+(pi*r*r));
@@ -34,7 +34,7 @@ class Rectangle implements prop
 {
     double l,b;
     Scanner sc = new Scanner(System.in);
-    @Override
+    
     public void getdata()
     {
         System.out.println("Enter the length of the rectangle:");
@@ -42,12 +42,12 @@ class Rectangle implements prop
         System.out.println("Enter the breadth of the rectangle:");
         b = sc.nextDouble();
     }
-    @Override
+    
     public void area()
     {
         System.out.println("Perimeter of a rectangle: "+(l*b));
     }
-    @Override
+     
     public void perimeter()
     {
         System.out.println("Perimeter of a rectangle: "+(2*(l+b)));
